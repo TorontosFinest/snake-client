a62b0aa319cf73b7a5d884ea44c6cc0145278ffc
TorontosFinest <abdialicanada20@gmail.com>
2021-12-01 21:55:00 +0000

    added movement

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -6,11 +9,22 @@ const setupInput = function () {
   stdin.on("data", handleUserInput);
   return stdin;
 };
+
 const handleUserInput = function (input) {
-  const stdout = process.stdout;
   if (input === "\u0003") {
-    stdout.write("Exited Game. Adios!\n");
     process.exit();
+  }
+  if (input === "w") {
+    connection.write("Move: up");
+  }
+  if (input === "a") {
+    connection.write("Move: left");
+  }
+  if (input === "s") {
+    connection.write("Move: down");
+  }
+  if (input === "d") {
+    connection.write("Move: right");
   }
 };
 
